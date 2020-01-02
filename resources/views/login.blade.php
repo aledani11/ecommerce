4c19100cbb8265d662e68f64df42372d38cabf31
Alexander <alex42daniel@gmail.com>
2020-01-02 14:44:37 -0300

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="css/core-style.css">
+<html>
+<body>
+<form action="#" method="post">
+<div class="container col-md-4">
+    <div class="row" >
+
+            <div class="row" style="margin:auto; border:1px solid #e8e8e8;">
+
+                <div class="col-md-12 mb-3 mt-3">
+                    <div class="mb-30" style="display:block;text-align:center">
+                        <h5>Login</h5>
+                    </div>
+                </div>
+
+                <div class="col-md-12 mb-3">
+                    <label for="state">Province</label>
+                    <input type="text" class="form-control" id="state" value="">
+                </div>
+                <div class="col-md-12 mb-4">
+                    <label for="email_address">Email Address</label>
+                    <input type="email" class="form-control" id="email_address" value="">
+                </div>
+                    
+                <div class="col-md-12 mb-2">
+                 <a href="#" class="btn essence-btn" style="">Place Order</a>
+                 </div>
+                <!--
+                <a href="#">olvido la contraseña</a>
+                <a href="#">registrarse</a> -->
+
+            </div>
+            
+        </div>
+    </div>
+</form>
+</body>
+</html>
